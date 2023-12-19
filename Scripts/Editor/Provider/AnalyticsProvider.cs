94157cea6d2561fceb8e8b188e967fce435dd7f5
KleinerHacker <Christoph.Pfeiffer.Berlin@freenet.de>
2023-12-19 17:49:52 +0100

    insert disabling of analytics

---
+++ Scripts/Editor/Provider/AnalyticsProvider.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEditorEx.Editor.editor_ex.Scripts.Editor.Utils;
+
+namespace UnityAnalyticsEx.Editor.analytics_ex.Scripts.Editor.Provider
+{
+    public sealed class AnalyticsProvider : SettingsProvider
+    {
+        #region Static Area
+
+        [SettingsProvider]
+        public static SettingsProvider CreateSettingsProvider()
+        {
+            return new AnalyticsProvider();
+        }
+
+        #endregion
+
+        public AnalyticsProvider() : base("Project/Player/Analytics", SettingsScope.Project, new[] { "analytics" })
+        {
+        }
+
+        public override void OnGUI(string searchContext)
+        {
+            ExtendedEditorGUILayout.SymbolField("Enable Analytics Support", "PCSOFT_ANALYTICS");
+        }
+    }
+}
--- Scripts/Runtime/UnityAnalyticsExEvents.cs
+++ Scripts/Runtime/UnityAnalyticsExEvents.cs
@@ -8,10 +8,15 @@ namespace UnityAnalyticsEx.Runtime.analytics_ex.Scripts.Runtime
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
         public static void Initialize()
         {
+#if PCSOFT_ANALYTICS
             Debug.Log("Initialize analytics system");
             var allow = AnalyticsEx.IsAnalyticsActivatedEx.GetValueOrDefault(false);
             Analytics.enabled = allow;
             Debug.Log("> Analytics: " + allow);
+#else
+            Debug.Log("> Analytics deactivated");
+            Analytics.enabled = false;
+#endif
         }
     }
 }
